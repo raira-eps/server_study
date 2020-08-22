4618062a3076fa721f754ae100e4e7e19bcd1169
raira-eps <atf65aaa935b@gmail.com>
2020-08-22 22:04:30 +0900

    isolate the function of createServer

--- node-app/app.js
+++ node-app/app.js
@@ -1,9 +1,20 @@
 const http = require('http');
+const fs = require('fs');
 
-var server = http.createServer(
-    (request,response) => {
-        response.end('Hello wprld');
-    }
-);
+var server = http.createServer(getFromClient);
 
 server.listen(4500);
+console.log('Server start!');
+
+
+function getFromClient(req,res){
+    request = req;
+    response = res;
+    fs.readFile('./note.html', 'UTF-8',
+        (error, data) => {
+            response.writeHead(200, {'Content-Type': 'text/html'});
+            response.write(data);
+            response.end();
+        }
+    );
+}
