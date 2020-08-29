1ee44f7ee9efa41cad8b5af15210e2b39467ea93
raira-eps <atf65aaa935b@gmail.com>
2020-08-29 10:23:51 +0900

    Add body-parser package, and fixed index.ejs

--- express-app/index.js
+++ express-app/index.js
@@ -5,14 +5,32 @@ var app = express();
 
 app.engine('ejs', ejs.renderFile);
 
-app.get('/', (req, res) => {
+app.use(express.static('public'));
+
+//top page
+app.get("/", (req, res) => {
+    var msg = "Top page";
+    var url = "/other?name=taro&pass=yamada";
     //index.ejsをレンダリングする
     res.render('index.ejs', {
         title:'Index',
-        content:'This is Express-app Top page'
+        content: msg,
+        link:{href:url, text:'※別のページに移動'},
     });
 });
 
+//other page
+app.get("/other", (req, res) => {
+    var name = req.query.name;
+    var pass = req.query.pass;
+    var msg = "あなたの名前は「"+ name +"」<br>パスワードは「"+ pass +"」です。";
+    //index.ejs をレンダリングする
+    res.render("index.ejs", {
+        title:"Other",
+        content: msg,
+        link:{href:"/", text:"※トップページへ移動"}
+    })
+})
 var server = app.listen(4500, () => {
     console.log('Start server port:4500');
 });
