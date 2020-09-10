2e1782a951c00e8f66b04330b5cd791fdde91d6f
raira-eps <atf65aaa935b@gmail.com>
2020-09-11 03:24:56 +0900

    fixed hello.js and hello.ejs. The connection with mysql_db is now possible.

--- ex-gen-app/routes/hello.js
+++ ex-gen-app/routes/hello.js
@@ -3,39 +3,44 @@ var router = express.Router();
 var mysql = require('mysql');
 
 var mysql_setting = {
-    host    :'localhost',
+    host    :'127.0.0.1',
     user    :'root',
-    password:'',
-    database:'my-nodeapp-db'
+    password:'root',
+    database:'my_nodeapp_db'
 }
 
 router.get('/', (req, res, next) => {
     //コネクションの用意
     var connection = mysql.createConnection(mysql_setting);
+    console.log('Connection Order');
     //データベースに接続
     connection.connect();
+    console.log('connecting...');
     //データを取り出す
     connection.query('SELECT * from mydata', function(error, results, fields){
         //データベースアクセス完了時の処理
+        console.log('Connection complete');
         if (error == null) {
+            console.log('connection error');
             var data = {title:'mysql', 
-                        cintent:results,
+                        content:results,
                        };
             res.render('hello', data);
         }
     });
     //接続を解除
     connection.end();
+    console.log('End of Conecction');
 });
 
-router.post('/post', (req, res, next) => {
-    var msg = req.body['message'];
-    req.session.message = msg;
-    var data = {
-        title:"Hello",
-        content:"Last message: " + req.session.message,
-    };
-    res.render('hello', data);
-})
+//router.post('/post', (req, res, next) => {
+// var msg = req.body['message'];
+//   req.session.message = msg;
+//    var data = {
+//        title:"Hello",
+//        content:"Last message: " + req.session.message,
+//    };
+//    res.render('hello', data);
+//})
 
 module.exports = router;
