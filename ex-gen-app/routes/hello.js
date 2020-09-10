67793e4fcb546c4cde261821a9f09e713d144655
raira-eps <atf65aaa935b@gmail.com>
2020-09-11 04:52:11 +0900

    Add /add processting to hello.js

--- ex-gen-app/routes/hello.js
+++ ex-gen-app/routes/hello.js
@@ -33,6 +33,34 @@ router.get('/', (req, res, next) => {
     console.log('End of Conecction');
 });
 
+//------------------------------------------------------------------------------//
+
+//新規作成ページへのアクセス
+router.get('/add', (req, res, next) => {
+    var data = {
+        title:'Hello/add',
+        content:'新しいレコードを追加'
+    }
+    res.render('hello/add', data);
+});
+
+//新規作成フォーム送信の処理
+router.post('/add', (req, res, next) => {
+    var nm = req.body.name;
+    var ml = req.body.mail;
+    var ag = req.body.age;
+    var data = {'name': nm, 'mail': ml, 'age': ag,};
+
+    //データベースの設定情報
+    var connection = mysql.createConnection(mysql_setting);
+    //データベースに接続
+    connection.connect();
+    //データを取り出す
+    connection.query('insert into mydata set ?', data, function(error, results, fields){
+        res.render('/hello');
+    });
+});
+
 //router.post('/post', (req, res, next) => {
 // var msg = req.body['message'];
 //   req.session.message = msg;
